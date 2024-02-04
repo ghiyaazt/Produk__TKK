584ba8d39799dc20b1cf430ce08c4d59b7823a3d
ghiyaazt <redgmaing66@gmail.com>
2024-02-04 21:49:55 +0700

    update

---
+++ Prosedur Kerja/variabel.php
@@ -0,0 +1,9 @@
+<?php
+    $nim="E32231768";
+    $nama="ACHMAD GHIYAZT SURYO ANTORO";
+    $prodi="Teknik Komputer";
+    echo "Selamat Praktikum";
+    echo $nim;
+    echo $nama;
+    echo $prodi;
+?>
